33fd1293199502a3a852e8a0b8c8f6747eb0fba6
whdiaz <whdiaztorres@gmail.com>
2024-01-14 16:11:49 -0300

    assignament prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess;
+        int guessCount = 0;
+        String response;
+        
+        do{
+
+        Random randomMagicNumber = new Random();
+        int magicNumber = randomMagicNumber.Next(1,100);
+
+        
+        
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("Congratulations! You guessed it!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+
+        } while (guess != magicNumber);
+        
+        Console.WriteLine($"Tentatives {guessCount}");
+
+        Console.Write("Do you want to play again?: ");
+        response = Console.ReadLine();
+        }while (response == "yes");
     }
 }
