b36f23b78aa5d58aa422f035d7f0225ac3ce68b4
whdiaz <whdiaztorres@gmail.com>
2024-01-14 01:20:58 -0300

    assignament prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,77 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        String letter = "";
+        String sing = "";
+
+        Console.Write("Please, enter your grade here: ");
+        string gradeStudent = Console.ReadLine();
+
+        int grade = int.Parse(gradeStudent);
+
+        if (grade >= 90){
+
+            letter = "A";
+            
+        }
+
+        else if (grade >= 80 && grade < 90){
+            letter = "B";
+            
+
+        }
+
+        else if (grade >= 70 && grade < 80){
+            letter = "C";
+            
+        }
+
+        else if (grade >= 60 && grade < 70){
+            letter = "D";
+           
+        }
+
+        else if (grade < 60){
+            letter = "F";
+            
+        }
+        else {
+            Console.WriteLine("Grade not valid");
+        }
+
+       
+
+       
+
+        int totalSing = grade % 10;
+
+        if (totalSing >=7){
+            sing = "+";
+        }
+        else {
+            sing = "-";
+        }
+
+        if (letter == "A" && totalSing >7) {
+            sing = " ";
+        }
+        else if (letter == "F" && totalSing <= 3){
+            sing = " ";
+        }
+
+         Console.WriteLine($" Your Grade is {letter}{sing}");
+
+         if (grade >= 70){
+            Console.WriteLine("Congratulations! you are in.");
+        }
+
+        
+
+
+        
     }
 }
