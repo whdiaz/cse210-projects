c1e5dbd061214d53966ea05f7a778275f77e872c
whdiaz <whdiaztorres@gmail.com>
2024-01-21 02:27:36 -0300

    assignament prep 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2026;
+        job1._endYear = 2036;
+
+        job2._jobTitle = "CEO";
+        job2._company = "Microsoft";
+        job2._startYear = 2036;
+        job2._endYear = 2045;
+
+        job1.DisplayInformationAboutJob();
+        job2.DisplayInformationAboutJob();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Willian Diaz";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayInformationAboutResume();
+        
     }
 }
